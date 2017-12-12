a73d2b5c8125dcae324c2bfe733b12d6aaa964d5
ronoc4 <conorbudge555@gmail.com>
2017-12-11 20:38:33 -0600

    Initial commit

---
+++ src/components/DetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card-image">
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'detail-view'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PostView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="waiting">
+      Not yet available
+    </div>
+</template>
+
+<script>
+  /* eslint-disable indent */
+
+  export default {
+        name: 'post-view'
+    }
+</script>
+
+<style scoped>
+.waiting {
+  padding: 10px;
+  color: #555;
+}
+</style>
